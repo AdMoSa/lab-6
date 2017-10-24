72da928d0fe15f60d1336a5b8516043cb72c211e
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-10-24 16:57:48 +0530

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -1,32 +1,50 @@
 # lab-6
 // question 7
 #include <iostream>
-#include<stdio.h>
 #include<string.h>
 using namespace std;
+int length(char*x1)
+{
+  char le;int de=0,i=0;
+  for(i =0; *(x1+i)!='\0';i++)
+  {
+    de++;
+  }
+  return de;
+}
+
+
+
+char *copy(char *x1,char *x2)
+{
+   x2=x1; 
+/*int a=length(x1);
+  cout<<a;
+  for(int i=0;i<a;i++)
+  *(x2+i)=*(*x1+i);*/
+  return x2;
+}
 int main()
 {
     char str1[10]="hello";
     char str2[10]=" Akash";
     char str3[10];
-    strcpy(str3,str2);
-    cout<<"srt3 becomes : "<<str3<<endl;
+    char *a1;
+    a1=str1;
+    char *a2=str2;
+    char *a3;
+    cout<<"length of the string : "<< length(a1);
+    char *d=copy(a1,a3);
+    cout<<*d;
+  
+    /* strcpy(*a3,*a2);
+    cout<<"*a3 becomes : "<<*a3<<endl;
     strcat(str1,str2);
     cout<<"when using strcat(str1,str2) : "<<str1<<endl;
     cout<<"Using strlen(str1) : "<<strlen(str1);
     cout<<endl<<"Using strcmp(str3,str2) : "<<strcmp(str3,str2);
     cout<<endl<<"Using strchr(str1,'l') : "<<strchr(str1,'l');
-    cout<<endl<<"Using strstr(str1,str2) : "<<strstr(str1,str2);
+    cout<<endl<<"Using strstr(str1,str2) : "<<strstr(str1,str2);*/
 
     return 0;
 }
-
-
-/ * output of the program 
-    str3 becomes : Akash
-    when using strcat(str1,str2) : hello Akash
-    Using strlen(str1) : 11
-    Using strcmp(str3,str2) : 0
-    Using strchr(str3,'l') : llo Akash
-    Using strstr(str1,str2) :  Akash
-*/    

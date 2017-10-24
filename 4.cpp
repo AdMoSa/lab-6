7f77763002b2c46d99c0f21786f454a2e415de0b
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-10-24 15:21:55 +0530

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,14 +1,13 @@
 # lab-6
 // question 4
 #include <iostream>
-#include<stdio.h>
 #include<string.h>
 using namespace std;
 int main()
 {
     char name[20];
     cout<<"Enter your name : "<<endl;
-     gets(name);
+     cin.getline(name,20);
     cout<<endl<<"Normal index method : "<<endl;
     for(int j=0;j<strlen(name);j++)
      cout<<name[j]<<endl;
